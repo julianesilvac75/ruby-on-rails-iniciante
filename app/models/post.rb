d063559d79aa33b601bec2555bf99369757f1075
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-20 21:15:07 -0300

    exemplo de ação before_save na model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   validates :title, length: { minimum: 5 }
   validates :description, length: { minimum: 15, maximum: 30 }
 
-  
+  # before_save do
+  #   self.title = self.title + " Juliane"
+  # end
 end
