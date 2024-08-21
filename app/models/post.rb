37d641dc5480ea7764ed018b6e661c591d188c63
Juliane Cardoso <julianesilvac75@gmail.com>
2024-08-20 21:09:09 -0300

    cria validações de obrigatoriedade, tamanho minimo e maximo para criação de posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :description, length: { minimum: 15, maximum: 30 }
+
+  
 end
